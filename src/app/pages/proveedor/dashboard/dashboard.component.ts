940781f602abe4be050ba5633eae178439448e06
Carlos Garcia <josecarlosgarciadiaz@gmail.com>
2024-12-24 13:48:41 -0600

    upd

--- src/app/pages/proveedor/dashboard/dashboard.component.ts
+++ src/app/pages/proveedor/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
-import { AppsService } from '../../../shared/services/apps.service';
+import { Component,Input, OnInit,ElementRef } from '@angular/core';
+import tableData  from '../../../../assets/data/pages/table-data.json';
 import { NzModalService } from 'ng-zorro-antd/modal';
-import { ProjectList } from '../../../shared/interfaces/project-list.type';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,125 +8,177 @@ import { ProjectList } from '../../../shared/interfaces/project-list.type';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
-  view: string = 'cardView';
-  newProject: boolean = false;
-  projectListRaw: ProjectList[];
-  projectList: any[] = [];
-  searchQuery: string = '';
-  isLoading = true;
   showContent = false;
-  showContentShorting = false;
-  startValue: Date | null = null;
-  endValue: Date | null = null;
-  selectedSort: string = 'all';
-  filteredProjects: any[] = [];
+  upcomingEvent: any;
+  tabData: { key: string; label: string }[];
+  sellingTab: string = 'today';
 
-  constructor(private projectListSvc: AppsService, private modalService: NzModalService) {}
-
-  ngOnInit(): void {
-    this.filteredProjects = [...this.projectList];
-    this.filterProjects(); // Filter projects initially
-
-    this.projectListSvc.getProjectListJson().subscribe(data => {
-      this.projectListRaw = data;
-      this.projectList = data;
-
-      this.sortProjects(this.selectedSort); // Sort projects based on the selected sort type
-    });
-
-    // Set default selection to "All"
-    this.selectedSort = 'all';
-
-    // Simulate loading time
-    this.loadData();
-  }
-
-    // Simulate loading data
-    loadData() {
-      // Simulate an asynchronous data loading operation
-      setTimeout(() => {
-        this.isLoading = false;
-        this.showContent = true;
-      }, 500);
-    }
-  // Filter the projects based on the search query
-  filterProjects() {
-    if (this.searchQuery.trim() === '') {
-      this.filteredProjects = [...this.projectList];
-    } else {
-      const query = this.searchQuery.toLowerCase().trim();
-      this.filteredProjects = this.projectList.filter(item => item.project.toLowerCase().includes(query));
-    }
-  }
-
-  // Sort the projects based on the selected sort type
-  sortProjects(sortType: string) {
-    this.selectedSort = sortType;
-    this.isLoading = true; // Enable loading effect
-    this.showContentShorting = false;
-
-    if (sortType === 'all') {
-      // Show all projects
-      this.filteredProjects = [...this.projectList];
-      this.isLoading = false; // Disable loading effect
-      this.showContentShorting = true;
-    } else {
-      // Filter projects based on sortType
-      setTimeout(() => {
-        this.filteredProjects = this.projectList.filter(item => item.status === sortType);
-        this.isLoading = false; // Disable loading effect
-        this.showContentShorting = true;
-      }, 500); // Simulate a delay for sorting
-    }
-  }
-
-  // Show modal for creating a new project
-  showNewProject(newProjectContent: TemplateRef<{}>) {
-    const modal = this.modalService.create({
-      nzTitle: 'Create New Project',
-      nzContent: newProjectContent,
-      nzFooter: [
+  constructor(private modalService: NzModalService, private elementRef: ElementRef) {
+    this.upcomingEvent = {
+      "today": [
         {
-          label: 'Create Project',
-          type: 'primary',
-          onClick: () =>
-            this.modalService.confirm({
-              nzTitle: 'Are you sure you want to create this project?',
-              nzOnOk: () => this.modalService.closeAll()
-            })
+          "id": 1,
+          "type": "primary",
+          "title": "Planning for new dashboard wireframe and prototype design",
+          "date": "19",
+          "month": "Mar",
+          "time": "08:30 AM"
+        },
+        {
+          "id": 2,
+          "type": "secondary",
+          "title": "International Web Conference 2021",
+          "date": "19",
+          "month": "Mar",
+          "time": "09:30 AM"
+        },
+        {
+          "id": 3,
+          "type": "info",
+          "title": "Dribble Designer Meetup",
+          "date": "19",
+          "month": "Mar",
+          "time": "10:30 AM"
+        },
+        {
+          "id": 4,
+          "type": "warning",
+          "title": "Dribble Designer Meetup",
+          "date": "19",
+          "month": "Mar",
+          "time": "11:30 AM"
         }
       ],
-      nzWidth: 620
+      "week": [
+        {
+          "id": 1,
+          "type": "primary",
+          "title": "Planning for new dashboard wireframe and prototype design",
+          "date": "12",
+          "month": "Sep",
+          "time": "08:30 AM"
+        },
+        {
+          "id": 2,
+          "type": "info",
+          "title": "International Web Conference 2021",
+          "date": "16",
+          "month": "Sep",
+          "time": "09:30 AM"
+        },
+        {
+          "id": 3,
+          "type": "secondary",
+          "title": "Dribble Designer Meetup",
+          "date": "15",
+          "month": "Sep",
+          "time": "10:30 AM"
+        },
+        {
+          "id": 4,
+          "type": "warning",
+          "title": "Dribble Designer Meetup",
+          "date": "13",
+          "month": "Sep",
+          "time": "11:30 AM"
+        }
+      ],
+      "month": [
+        {
+          "id": 1,
+          "type": "primary",
+          "title": "Planning for new dashboard wireframe and prototype design",
+          "date": "24",
+          "month": "Apr",
+          "time": "08:30 AM"
+        },
+        {
+          "id": 2,
+          "type": "secondary",
+          "title": "International Web Conference 2021",
+          "date": "24",
+          "month": "Apr",
+          "time": "09:30 AM"
+        },
+        {
+          "id": 3,
+          "type": "info",
+          "title": "Dribble Designer Meetup",
+          "date": "24",
+          "month": "Apr",
+          "time": "10:30 AM"
+        },
+        {
+          "id": 4,
+          "type": "warning",
+          "title": "Dribble Designer Meetup",
+          "date": "28",
+          "month": "Apr",
+          "time": "11:30 AM"
+        }
+      ]
+    };
+    this.tabData = [
+      { key: 'today', label: 'Today' },
+      { key: 'week', label: 'Week' },
+      { key: 'month', label: 'Month' }
+    ];
+  }
+
+ //Tabs
+ @Input() componentId: string;
+  //Tabs
+  handleClick(tab: string): void {
+    this.sellingTab = tab;
+    const storageKey = `sellingTab_${this.componentId}`; // Use a unique key for each component
+    localStorage.setItem(storageKey, tab);
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+    
+    const storageKey = `sellingTab_${this.componentId}`; // Use the same unique key as in handleClick
+    const storedTab = localStorage.getItem(storageKey);
+    if (storedTab) {
+      this.sellingTab = storedTab;
+    }
+  }
+
+  loadData() {
+    this.showContent = true;
+    }
+
+  // Method to open the edit modal
+  openEditModal(product: any): void {
+    const modalRef = this.modalService.create({
+      nzTitle: 'Update Product Title',
+      //nzContent: EditModalComponent,
+      nzFooter: [
+        {
+          label: 'Update Event',
+          type: 'primary',
+          onClick: () => {
+            // Update the product title
+            product.title = 'Titulo';
+            modalRef.destroy();
+          }
+        }
+      ]
     });
   }
 
-  // Checkbox event handler
-  log(value: string[]): void {
-  }
+  removeEvent(index: number): void {
+    const selectedTab = this.tabData.find(tab => tab.key === this.sellingTab);
+    if (selectedTab) {
+      const tabKey = selectedTab.key;
+      this.upcomingEvent[tabKey].splice(index, 1);
 
-  // Calendar
-  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
-  disabledStartDate = (startValue: Date): boolean => {
-    if (!startValue || !this.endValue) {
-      return false;
-    }
-    return startValue.getTime() > this.endValue.getTime();
-  };
-
-  disabledEndDate = (endValue: Date): boolean => {
-    if (!endValue || !this.startValue) {
-      return false;
-    }
-    return endValue.getTime() <= this.startValue.getTime();
-  };
-
-  handleStartOpenChange(open: boolean): void {
-    if (!open) {
-      this.endDatePicker.open();
+      // Remove the corresponding <tr> element from the DOM
+      const trElements = this.elementRef.nativeElement.querySelectorAll('.group');
+      if (trElements.length > index) {
+        trElements[index].remove();
+      }
     }
   }
 
-  handleEndOpenChange(open: boolean): void {
-  }
 }
